3ed6df8d843cb3e919782dfae8e2b821841ab1f6
Taek <rudxor5026@gmail.com>
2020-06-11 16:39:17 +0900

    Updates

--- precache-manifest.b8f4ccae72ec8796abcf9691b58ce432.js
+++ precache-manifest.b8f4ccae72ec8796abcf9691b58ce432.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c8598802c12afc3a2ab2",
+    "revision": "a32e1278d53643fa4b6e",
     "url": "/react-calculator/static/css/main.5c049a78.chunk.css"
   },
   {
-    "revision": "c8598802c12afc3a2ab2",
-    "url": "/react-calculator/static/js/main.c8598802.chunk.js"
+    "revision": "a32e1278d53643fa4b6e",
+    "url": "/react-calculator/static/js/main.a32e1278.chunk.js"
   },
   {
     "revision": "b55a45ea74585038c4e7",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
   },
   {
-    "revision": "769b518a5890605e9ad677a6466690ed",
+    "revision": "641188fea74db45c6b0f99cdfe707a7a",
     "url": "/react-calculator/index.html"
   }
 ];
